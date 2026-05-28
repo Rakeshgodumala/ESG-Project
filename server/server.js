dc926ff3fd78d772d887da3a37d59ae1b4cc84a4
Rakesh Godumala <86297780+Rakeshgodumala@users.noreply.github.com>
2026-05-28 22:32:19 +0530

    fixed cord

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use("/api/upload", require("./routes/uploadRoutes"));
